fcb53bef36c0860f82a36268d84f385f00645057
hidefall <hidefall@gmail.com>
2018-09-08 21:18:47 +0300

    issues

---
+++ wp-content/uploads/cache/wpml/twig/f5/f5113dd73f2e1edbe914a521a3f51cde7fe7173adc769f5e086a9ea90ca7042b.php
@@ -0,0 +1,45 @@
+<?php
+
+/* save-notification.twig */
+class __TwigTemplate_88ba13a2985d891bc1e0621a67f251d66a31e30a779c31ef1b8e7f3b889c023c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<span class=\"js-wpml-ls-messages wpml-ls-messages\"></span><span class=\"spinner\"></span>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "save-notification.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "save-notification.twig", "/var/www/html/zimba/wp-content/plugins/sitepress-multilingual-cms/templates/language-switcher-admin-ui/save-notification.twig");
+    }
+}
